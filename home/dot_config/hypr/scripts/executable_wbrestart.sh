b147224b19f04a170b641918a6ed803afe19c1fd
xNinjaKittyx <xNinjaKittyx@users.noreply.github.com>
2026-01-10 17:03:46 -0800

    feat: new cursor, migrating to wleave, adding some matugen support. some xdg fixes.

--- home/dot_config/hypr/scripts/executable_wbrestart.sh
+++ home/dot_config/hypr/scripts/executable_wbrestart.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
-killall -9 swaync
-killall -9 waybar
+pkill -9 swaync
+pkill -9 waybar
+pkill -9 linux-wallpaperengine
 
 swaync &
 waybar &
+linux-wallpaperengine 3504312917 --screen-root DP-1 &
--- home/dot_config/hypr/scripts/executable_wlogout.sh
+++ home/dot_config/hypr/scripts/executable_wlogout.sh
@@ -4,19 +4,21 @@ B_1080=1700
 L_1080=3000
 R_1080=3000
 
-# Check if wlogout is already running
-if pgrep -x "wlogout" > /dev/null; then
-    pkill -x "wlogout"
+# Check if wleave is already running
+if pgrep -x "wleave" > /dev/null; then
+    pkill -x "wleave"
     exit 0
 fi
 
-# Detect monitor resolution and scaling factor
-resolution=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .height / .scale' | awk -F'.' '{print $1}')
-hypr_scale=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .scale')
-wlogout -C $HOME/.config/wlogout/nova.css -l $HOME/.config/wlogout/layout \
-    --protocol layer-shell -b 4 \
-    -T $(awk "BEGIN {printf \"%.0f\", $A_1080 * 1080 * $hypr_scale / $resolution}") \
-    -B $(awk "BEGIN {printf \"%.0f\", $B_1080 * 1080 * $hypr_scale / $resolution}") \
-    -L $(awk "BEGIN {printf \"%.0f\", $L_1080 * 1080 * $hypr_scale / $resolution}") \
-    -R $(awk "BEGIN {printf \"%.0f\", $R_1080 * 1080 * $hypr_scale / $resolution}") \
-    &
+wleave -C $HOME/.config/wleave/nova.css -l $HOME/.config/wleave/layout.json
+
+# # Detect monitor resolution and scaling factor
+# resolution=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .height / .scale' | awk -F'.' '{print $1}')
+# hypr_scale=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .scale')
+# wleave -C $HOME/.config/wleave/nova.css -l $HOME/.config/wleave/layout \
+#     --protocol layer-shell -b 4 \
+#     -T $(awk "BEGIN {printf \"%.0f\", $A_1080 * 1080 * $hypr_scale / $resolution}") \
+#     -B $(awk "BEGIN {printf \"%.0f\", $B_1080 * 1080 * $hypr_scale / $resolution}") \
+#     -L $(awk "BEGIN {printf \"%.0f\", $L_1080 * 1080 * $hypr_scale / $resolution}") \
+#     -R $(awk "BEGIN {printf \"%.0f\", $R_1080 * 1080 * $hypr_scale / $resolution}") \
+#     &
